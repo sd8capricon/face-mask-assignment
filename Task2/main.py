edcf39cdf23ed9964c71be5da56a272f9a646b3e
sd8capricon <sd8capricon@gmail.com>
2024-10-02 16:39:57 +0530

    split tasks

---
+++ Task2/main.py
@@ -0,0 +1,39 @@
+import os
+
+from face_detection_tracking import (
+    load_video,
+    save_video,
+    make_detections,
+    make_detections_with_tracking,
+)
+
+VIDEO_PATH = "data"
+OUT_PATH = "out"
+
+video_filenames = [
+    "Test_video1.mp4",
+    "Test_video2.mp4",
+    "Test_video3.mp4",
+]
+
+if not os.path.exists("out/with_tracking"):
+    os.makedirs("out/with_tracking")
+
+for i, video_name in enumerate(video_filenames):
+    save_as = f"output_video{i+1}"
+    video = load_video(os.path.join(VIDEO_PATH, video_name))
+
+    # Make detections without tracking
+    video_wo_tracking = make_detections(video, face_confidence=0.4)
+
+    # save the video
+    save_video(video_wo_tracking, os.path.join(OUT_PATH, video_name))
+
+    video_w_tracking = make_detections_with_tracking(
+        video, detection_interval=60, face_confidence=0.4
+    )
+
+    # save the tracked video
+    save_video(
+        video_w_tracking, os.path.join(OUT_PATH, f"with_tracking/output _video{i+1}")
+    )
